a79ed90222aad943587baa01406b9ee23d86dff6
mitul adroja <mitul.adroja13@gmail.com>
2023-06-22 09:15:05 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Folks!</h1>
 <p>This repo is for DSA</p>
 `;
+
+/**
+ * Flatten array
+ */
+
+function flattenArray(arr) {
+  let flatArray = [];
+  arr.map((ele) => {
+    if (Array.isArray(ele)) {
+      flatArray = [...flatArray, ...flattenArray(ele)];
+    } else {
+      flatArray.push(ele);
+    }
+  });
+  return flatArray;
+}
+
+console.log(flattenArray([1, 2, [3, 4, [5, 6, [7, 8, 9]]]]));
